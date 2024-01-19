bd219dbcff27cb6b4d0645e373a1f41b944ec5d5
Akatsuki1910 <akatsukicirno@gmail.com>
2024-01-19 15:23:19 +0900

    Create Show.svelte

---
+++ src/lib/components/Show.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let show = true;
+</script>
+
+<div class:hide={!show}>
+  <slot />
+</div>
+
+<style lang="scss">
+  .hide {
+    display: none !important;
+  }
+</style>
